9196403fa2c905e62dd17296144d968153c2829b
Sanctus-Peter <sanctuspeter@gmail.com>
2022-12-26 07:32:48 +0100

    Updated car factory and added tire

--- car.py
+++ car.py
@@ -1,12 +1,16 @@
 from serviceable import Serviceable
 from battery.battery import Battery
 from engine.engine import Engine
+from tyre.tyre import Tyre
 
 
 class Car(Serviceable):
-    def __init__(self, engine: Engine, battery: Battery):
+    def __init__(self, engine: Engine, battery: Battery, tyre: Tyre):
         self.__engine = engine
         self.__battery = battery
+        self.__tyre = tyre
 
     def needs_service(self):
-        return self.__engine.needs_service() or self.__battery.needs_service()
+        return self.__engine.needs_service() or \
+            self.__battery.needs_service() or \
+            self.__tyre.needs_service()
--- carFactory.py
+++ carFactory.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from car import Car
 from battery import nubbin_battery, spindler_battery
 from engine import capulet_engine, sternman_engine, willoughby_engine
+from tyre import carrigan_tyre, octoprime_tyre
 
 
 class CarFactory:
@@ -11,50 +12,60 @@ class CarFactory:
             last_service_date: datetime,
             current_mileage: int,
             last_service_mileage: int,
+            tyre_wear_readings: list[float]
     ) -> Car:
         engine_type = capulet_engine.CapuletEngine(last_service_mileage, current_mileage)
         battery_type = spindler_battery.SpindlerBattery(last_service_date, current_date)
-        return Car(engine_type, battery_type)
+        tyre_type = carrigan_tyre.CarriganTyre(tyre_wear_readings)
+        return Car(engine_type, battery_type, tyre_type)
 
     @staticmethod
     def create_glissade(
             current_date: datetime,
             last_service_date: datetime,
             current_mileage: int,
-            last_service_mileage: int
+            last_service_mileage: int,
+            tyre_wear_readings: list[float]
     ) -> Car:
         engine_type = willoughby_engine.WilloughbyEngine(last_service_mileage, current_mileage)
         battery_type = spindler_battery.SpindlerBattery(last_service_date, current_date)
-        return Car(engine_type, battery_type)
+        tyre_type = octoprime_tyre.OctoprimeTyre(tyre_wear_readings)
+        return Car(engine_type, battery_type, tyre_type)
 
     @staticmethod
     def create_palindrome(
             current_date: datetime,
             last_service_date: datetime,
-            warning_light_on: bool
+            warning_light_on: bool,
+            tyre_wear_readings: list[float]
     ) -> Car:
         engine_type = sternman_engine.SternmanEngine(warning_light_on)
         battery_type = spindler_battery.SpindlerBattery(last_service_date, current_date)
-        return Car(engine_type, battery_type)
+        tyre_type = carrigan_tyre.CarriganTyre(tyre_wear_readings)
+        return Car(engine_type, battery_type, tyre_type)
 
     @staticmethod
     def create_rorschach(
             current_date: datetime,
             last_service_date: datetime,
             current_mileage: int,
-            last_service_mileage: int
+            last_service_mileage: int,
+            tyre_wear_readings: list[float]
     ) -> Car:
         engine_type = willoughby_engine.WilloughbyEngine(current_mileage, last_service_mileage)
         battery_type = nubbin_battery.NubbinBattery(last_service_date, current_date)
-        return Car(engine_type, battery_type)
+        tyre_type = carrigan_tyre.CarriganTyre(tyre_wear_readings)
+        return Car(engine_type, battery_type, tyre_type)
 
     @staticmethod
     def create_thovex(
             current_date: datetime,
             last_service_date: datetime,
             current_mileage: int,
-            last_service_mileage: int
+            last_service_mileage: int,
+            tyre_wear_readings: list[float]
     ) -> Car:
         engine_type = capulet_engine.CapuletEngine(current_mileage, last_service_mileage)
         battery_type = nubbin_battery.NubbinBattery(last_service_date, current_date)
-        return Car(engine_type, battery_type)
+        tyre_type = carrigan_tyre.CarriganTyre(tyre_wear_readings)
+        return Car(engine_type, battery_type, tyre_type)
